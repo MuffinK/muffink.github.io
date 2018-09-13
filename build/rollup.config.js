7a407748192641ec90879b6b77020ac79960d341
muffin <jinxz_k@live.com>
2018-09-13 10:07:31 +0800

    package

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -3,7 +3,8 @@ import typescript from 'rollup-plugin-typescript2';
 import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import buble from 'rollup-plugin-buble'; // Transpile/polyfill with reasonable browser support
-
+import rollupPluginTypescriptPathMapping from 'rollup-plugin-typescript-path-mapping'
+ 
 
 import alias from 'rollup-plugin-alias';
 export default {
@@ -11,6 +12,7 @@ export default {
     output: {
       name: 'singleNumberInput',
       exports: 'named',
+      globals: ['Vue']
     },
     external: [ 'vue' ],
     plugins: [
@@ -18,42 +20,17 @@ export default {
         css: true, // Dynamically inject css as a <style> tag
         compileTemplate: true, // Explicitly convert template to render function
       }),
-          alias({
-            resolve: ['.js'],
-            "vue-class-component": 'vue-class-component/lib/index'
-          }),
+         rollupPluginTypescriptPathMapping({
+            baseUrl: './',
+            paths: {
+                'vue-class-component': ['vue-class-component/lib/index']
+            },
+            outDir: 'dist',
+            rootDir: 'src'
+        }),
       nodeResolve({
         jsnext: true,
-        main: false,
-        // jail: '/node_modules/vue-property-decorator', // Default: '/'
-      }),
-      commonjs({
-        // non-CommonJS modules will be ignored, but you can also
-        // specifically include/exclude files
-        include: ['node_modules/vue-property-decorator', 'node_modules/vue-class-component'],  // Default: undefined
-        // exclude: [ 'node_modules/foo/**', 'node_modules/bar/**' ],  // Default: undefined
-        // these values can also be regular expressions
-        // include: /node_modules/
-  
-        // search for files other than .js files (must already
-        // be transpiled by a previous plugin!)
-        extensions: [ '.js', '.vue' ],  // Default: [ '.js' ]
-  
-        // if true then uses of `global` won't be dealt with by this plugin
-        ignoreGlobal: false,  // Default: false
-  
-        // if false then skip sourceMap generation for CommonJS modules
-        sourceMap: false,  // Default: true
-  
-        // explicitly specify unresolvable named exports
-        // (see below for more details)
-        // namedExports: { './node_modules/vue-class-component/dist/vue-class-component.common.js': ['Component'] },  // Default: undefined
-  
-        // sometimes you have to leave require statements
-        // unconverted. Pass an array containing the IDs
-        // or a `id => boolean` function. Only use this
-        // option if you know what you're doing!
-        // ignore: [ 'conditional-runtime-dependency' ]
+        main: false
       }),
       typescript(),
       buble() // Transpile to ES5x
